10d21c80d463aa1692ca6de1e1daa098f96bd758
Adinath Shelke <shelkeadinath3@gmail.com>
2023-02-15 19:28:29 +0530

    Created api routes and a post model

--- back-end/index.js
+++ back-end/index.js
@@ -2,7 +2,8 @@ import express from 'express'
 import * as dotenv from 'dotenv'
 import cors from 'cors'
 import connectDB from './mongodb/connect.js';
-
+import postRoutes from "./routes/postRoutes.js";
+import dalleRoutes from "./routes/dalleRoutes.js";
 
 
 dotenv.config();
@@ -10,6 +11,12 @@ dotenv.config();
 const app = express()
 app.use(cors())
 app.use(express.json({"limit":"50mb"}))
+
+//Creating route to post
+app.use("/api/v1/post",postRoutes)
+
+//Creating route to open ai's dalle api
+app.use("/api/v1/dalle",dalleRoutes)
 
 app.get('/',async (req,res) => {
     res.send("Hello from imaginary")
