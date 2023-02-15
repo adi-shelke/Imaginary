335f59d845d0a57e4ed1bcd984658d6c4786a291
Adinath Shelke <shelkeadinath3@gmail.com>
2023-02-15 12:05:19 +0530

    Created a connection to the mongo atlas

--- back-end/index.js
+++ back-end/index.js
@@ -1,6 +1,9 @@
 import express from 'express'
 import * as dotenv from 'dotenv'
 import cors from 'cors'
+import connectDB from './mongodb/connect.js';
+
+
 
 dotenv.config();
 
@@ -13,6 +16,13 @@ app.get('/',async (req,res) => {
 })
 
 const startServer = async () =>{
-    app.listen(8080, () => console.log("Server is running on port http://localhost:8080"))
+    try {
+        connectDB(process.env.MONGODB_URL)
+        app.listen(8080, () => console.log("Server is running on port http://localhost:8080"))
+    } catch (error) {
+        console.log(error)
+    }
 }
 startServer()
+
+
