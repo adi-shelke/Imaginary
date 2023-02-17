574dcf693b116b92caca05eff72710e5ba54a528
Adinath Shelke <shelkeadinath3@gmail.com>
2023-02-18 00:15:27 +0530

    added icons

--- front-end/src/components/Header.jsx
+++ front-end/src/components/Header.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { BrowserRouter, Link, Route, Routes } from 'react-router-dom'
 import {logo} from '../assets'
+import {logo2} from '../assets'
 
 const Header = () => {
   return (
     <header className="w-full flex justify-between items-center bg-white sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]">
       <Link to="/">
-        <img src={logo} alt="logo" className="w-28 object-contain" />
+        <img src={logo2} alt="logo" className="w-56 object-contain" />
       </Link>
       <Link
         to="/create-post"
